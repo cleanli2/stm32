e2d677ac75008282976a84b8004954bc881d370e
clean_li <clean_li@yahoo.com>
2021-07-10 21:20:05 +0800

    Uart baudrate change to 115200

--- src/common.c
+++ src/common.c
@@ -495,7 +495,7 @@ void main_init(void)
 
   delay_init();
 
-  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_BaudRate = 115200;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = USART_StopBits_1;
   USART_InitStructure.USART_Parity = USART_Parity_No;
