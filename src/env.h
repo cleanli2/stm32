700a9ec3669adf0e88be8d26a2c9806f93813764
clean_li <clean_li@yahoo.com>
2022-03-06 09:44:00 +0800

    spi flash damage handle

--- src/env.h
+++ src/env.h
@@ -2,13 +2,33 @@
 #define __ENV_H
 #include <stdint.h>
 
+//SPI Flash devide
+/*
+ * 0x000000
+ * None
+ * 0x0FE000
+ * ENV Help
+ * 0x0FF000
+ * ENV
+ * 0x100000
+ * Damaged
+ * 0x200000
+ */
+//spi flash damaged now 2022/3/6
+//ENV offset changed to OK flash addr
+#define SPI_FLASH_DAMAGED
+
 #define TOTAL_SPI_FLASH_SIZE 0x200000
 #define SPI_FLASH_SECTOR_SIZE 0x1000
 #define SECTORS_PER_ENV_BLOCK 1
 #define ENV_STORE_SIZE (SPI_FLASH_SECTOR_SIZE*SECTORS_PER_ENV_BLOCK)
 
+#ifdef SPI_FLASH_DAMAGED
+#define ENV_STORE_START_ADDR (TOTAL_SPI_FLASH_SIZE/2-ENV_STORE_SIZE)
+#else
 //env store at end of flash
 #define ENV_STORE_START_ADDR (TOTAL_SPI_FLASH_SIZE-ENV_STORE_SIZE)
+#endif //SPI_FLASH_DAMAGED
 #define ENV_HELP_STORE_START_ADDR (ENV_STORE_START_ADDR-ENV_STORE_SIZE)
 
 #define ENV_OK 0
