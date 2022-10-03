831cd8f2dad9e981e96c1c1affada2164329d957
clean_li <clean_li@yahoo.com>
2022-10-03 22:10:54 +0800

    auto alert set for alientek mini

--- src/ui.c
+++ src/ui.c
@@ -57,7 +57,7 @@ void main_ui_init(void*vp)
     ui_t* uif =(ui_t*)vp;
     (void)uif;
     lprintf_time("mainmenu\n");
-#ifndef ALIENTEK_MINI
+#ifdef RTC_8563
     if(check_rtc_alert_and_clear()){
         often_used_timer();
     }
@@ -80,7 +80,7 @@ void poff_ctd_ui_init(void*vp)
     (void)uif;
     common_ui_init(vp);
     lcd_lprintf(1, 20, 100, "Version:%s%s", VERSION, GIT_SHA1);
-#ifndef ALIENTEK_MINI
+#ifdef RTC_8563
     auto_time_alert_set(AUTO_TIME_ALERT_INC_MINS, 20, 140);
     auto_time_correct();
 #endif
@@ -515,16 +515,25 @@ void date_ui_init(void*vp)
     if(ENV_OK == get_env("HsAdj1Min", t)){
         lcd_lprintf(1, 320, 630, "AutoCorrect:%s", t);
     }
+#endif
+#ifdef RTC_8563
+#ifdef LARGE_SCREEN
+    if(check_rtc_alert_and_clear()){
+        often_used_timer();
+    }
+    auto_time_alert_set(AUTO_TIME_ALERT_INC_MINS, 260, 45);
+#else
     if(check_rtc_alert_and_clear()){
         often_used_timer();
     }
     auto_time_alert_set(AUTO_TIME_ALERT_INC_MINS, 260, 45);
 #endif
+#endif
     draw_clock_face(CLOCK_CX, CLOCK_CY);
     ui_buf[LAST_SEC_INDX]=60;
     ui_buf[LAST_MIN_INDX]=60;
     ui_buf[LAST_HOR_INDX]=60;
-#ifndef ALIENTEK_MINI
+#ifdef RTC_8563
     auto_time_correct();
 #endif
 }
