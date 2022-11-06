4e15b8b6f970f6058bdf6090263145029fe8741a
clean_li <clean_li@yahoo.com>
2022-11-06 17:41:13 +0800

    clean up adc code

--- src/adc.h
+++ src/adc.h
@@ -1,6 +1,14 @@
 #ifndef _ADC_H
 #define _ADC_H
 
+enum MY_ADC_TYPE {
+    VREF,
+    V4_2,
+    VIMEA,
+    MY_ADC_END,
+};
+
+#define V_REV_mv 2500
 #define BATT_MAX 4250
 #define BATT_LOW_ALERT 3550
 #define BATT_LOW_LIMIT 3400
