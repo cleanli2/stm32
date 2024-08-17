f3b911c785d96b429f9a3473c299bb498ffc1975
clean_li <clean_li@yahoo.com>
2024-08-18 01:56:06 +0800

    add cl sd

---
+++ src/cl_sd.c
@@ -0,0 +1,5 @@
+#include "cl_sd.h"
+
+uint32_t cl_sd_init()
+{
+}
---
+++ src/cl_sd.h
@@ -0,0 +1,12 @@
+#ifndef __CL_SD_H
+#define __CL_SD_H
+
+#define CL_SD_OK 0
+#define CL_SD_FAIL 1
+
+
+#include <stdint.h>
+
+uint32_t cl_sd_init();
+
+#endif
