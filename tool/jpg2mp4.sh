7c147d52ea7bb20ace0a72d07ab0a5c40c4fe5c8
clean_li <clean_li@yahoo.com>
2024-09-12 22:17:45 +0800

    update jpg2mp4.sh

--- tool/jpg2mp4.sh
+++ tool/jpg2mp4.sh
@@ -1,1 +1,8 @@
-ffmpeg -f image2 -i yuv%d.jpg  -vcodec libx264 -r 10 $1.mp4
+
+if [ -z $1 ]
+then
+	name="default"
+else
+	name=$1
+fi
+ffmpeg -f image2 -i yuv%d.jpg  -vcodec libx264 -r 10 $name.mp4
