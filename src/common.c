85df815ded133588b705935b98b5f3dfa1706a49
clean_li <clean_li@yahoo.com>
2024-12-22 17:38:18 +0800

    debug1:cam OK

--- src/common.c
+++ src/common.c
@@ -818,8 +818,8 @@ void main_init(void)
       lprintf_time("fs hw inited\n");
   }
   lprintf_time("SD init done\n");
-  lprintf_time("lcd init\n");
-  lcd_sueb_init(0);
+  //lprintf_time("lcd init\n");
+  //lcd_sueb_init(0);
   lprintf_time("NO lcd init.\n");
   //SD_LowLevel_Init();
 
