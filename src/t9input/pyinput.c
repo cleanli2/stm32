495b1407f3779f7ae951e111eb3ab585a7d8e3cb
CleanLi <clean_li@hotmail.com>
2025-02-13 21:54:17 +0800

    set his buf 200

--- src/t9input/pyinput.c
+++ src/t9input/pyinput.c
@@ -70,12 +70,12 @@ unsigned char str_match(unsigned char*str1,unsigned char*str2)
 }
 
 #define MOST_MB_SIZE 213
-#define DYN_HISTORY_SIZE 20
+#define DYN_HISTORY_SIZE 200
 #define DYN_MB_SIZE 213
 int dyn_his_p=0;
-const unsigned char const_dyn_mb[DYN_MB_SIZE]={"的是不人一这了你有个就在他我能功么来修炼也那都到们大法上中去要出它为可看讲说什以心时会多样种体还好高常想气所现家下没很身自西过事得东次层生真道些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只"};
-unsigned char dyn_mb[DYN_MB_SIZE]={0};
-unsigned char most_mb[MOST_MB_SIZE]={"些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只现家下没很身自西过事得东次层生真道些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只其问空许够实理别对而动题怎定质点意教"};
+const unsigned char const_dyn_mb[DYN_MB_SIZE+1]={"的是不人一这了你有个就在他我能功么来修炼也那都到们大法上中去要出它为可看讲说什以心时会多样种体还好高常想气所现家下没很身自西过事得东次层生真道些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只"};
+unsigned char dyn_mb[DYN_MB_SIZE+1]={0};
+unsigned char most_mb[MOST_MB_SIZE+1]={"些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只现家下没很身自西过事得东次层生真道些间给把正里着当佛子做己天因病后往性之开成发物用情候师学本呢和起化作只其问空许够实理别对而动题怎定质点意教"};
 py_index const_dyn_index={"dynch","words",2,213,(unsigned char*)const_dyn_mb};
 py_index dyn_index={"dynch","words",2,213,dyn_mb};
 py_index most_index={"dynch","words",2,213,most_mb};
