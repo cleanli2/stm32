6b38eac1ac4e3c9ab3fa94b2362409c725f53404
CleanLi <clean_li@hotmail.com>
2025-02-15 17:30:02 +0800

    chn24:test 4

--- src/cmd.c
+++ src/cmd.c
@@ -735,9 +735,10 @@ void lcdsuebstep(char *p)
         }
     }
     else if(cmdindex == 0x15){//
+        char tc[3]={0xc9, 0xee, 0};
         if(tmp>=2)
             p = str_to_hex(p, &para1);
-        GUI_DrawZikuFont24(400, 400, BLUE, WHITE, "深圆测试",para1);
+        GUI_DrawZikuFont24(400, 400, BLUE, WHITE, tc,para1);
     }
     else if(cmdindex == 0x16){//
     }
--- src/env.h
+++ src/env.h
@@ -53,7 +53,56 @@
 #define SPI_FLASH_LOG_START 0x414000
 #define SPI_FLASH_LOG_END 0x420000
 
-#else//ALIENTEK_MINI
+#elif defined SUNRISE
+//SPI Flash devide
+/*
+ * 0x000000
+ * None
+ * 0x07E000
+ * ziku16
+ * 0x0C0000
+ * ziku12
+ * 0x0EFEB0
+ * None
+ * 0x0F0000
+ * log
+ * 0x0FA000
+ * ENV Help
+ * 0x0FD000
+ * ENV
+ * 0x100000
+ * Not used
+ * 0x180000
+ * ziku24
+ * 0x200000
+ */
+//spi flash damaged now 2022/3/6
+//ENV offset changed to OK flash addr
+//
+#define SPI_FLASH_ZIKU24_START 0x180000
+#define SPI_FLASH_ZIKU16_START 0x7E000
+#define SPI_FLASH_ZIKU12_START 0xC0000
+
+#define SPI_FLASH_DAMAGED
+
+#define TOTAL_SPI_FLASH_SIZE 0x200000
+#define SPI_FLASH_SECTOR_SIZE 0x1000
+#define GET_SECTOR_ADDR(addr) ((addr)>>12)
+#define SECTORS_PER_ENV_BLOCK 3
+#define ENV_STORE_SIZE (SPI_FLASH_SECTOR_SIZE*SECTORS_PER_ENV_BLOCK)
+
+#ifdef SPI_FLASH_DAMAGED
+#define ENV_STORE_START_ADDR (TOTAL_SPI_FLASH_SIZE/2-ENV_STORE_SIZE)
+#else
+//env store at end of flash
+#define ENV_STORE_START_ADDR (TOTAL_SPI_FLASH_SIZE-ENV_STORE_SIZE)
+#endif //SPI_FLASH_DAMAGED
+#define ENV_HELP_STORE_START_ADDR (ENV_STORE_START_ADDR-ENV_STORE_SIZE)
+
+#define SPI_FLASH_LOG_START 0x0F0000
+#define SPI_FLASH_LOG_END 0x0FA000
+
+#else//
 //SPI Flash devide
 /*
  * 0x000000
