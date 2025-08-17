5f8a78a57ed22e7ed4d5c80a2f0e83ef83066f16
CleanLi <clean_li@hotmail.com>
2025-08-17 23:19:46 +0800

    asm optimize speed of sd recv

--- src/asm.s
+++ src/asm.s
@@ -6,6 +6,7 @@
 .global TIM3_IRQHandler
 .global USART1_IRQHandler
 .global atomic_inc
+.global rcvr_spi_multi
 .code 16
 .syntax unified
 
@@ -69,6 +70,48 @@ ldrex r2, [r1]
 add r2, #1
 strex r0, r2, [r1]
 pop {r1, r2}
+bx lr
+
+.type rcvr_spi_multi, function
+rcvr_spi_multi:
+push {r2-r3}
+
+/*r0=char*buff, r1=lens*/
+/*r3=base of spi, r2=tmp data*/
+ldr r3, =0x40013000
+
+/*while((SD_SPI->SR & SPI_I2S_FLAG_TXE) == RESET);*/
+wait_tx_done:
+nop
+ldrh r2, [r3, #8]
+uxth r2, r2
+and.w r2, r2, #2
+cmp r2, #0
+beq.n wait_tx_done
+
+/*SD_SPI->DR = 0xff;*/
+movs r2, #255
+strh r2, [r3, #12]
+
+/*while((SD_SPI->SR & SPI_I2S_FLAG_RXNE) == RESET);*/
+wait_rx_done:
+nop
+ldrh r2, [r3, #8]
+uxth r2, r2
+and.w r2, r2, #1
+cmp r2, #0
+beq.n wait_rx_done
+
+/* *(buff+i) = SD_SPI->DR; */
+ldrh r2, [r3, #12]
+uxth r2, r2
+strb r2, [r0, #0]
+adds r0, r0, #1
+subs r1, r1, #1
+cmp r1, #0
+bne.n wait_tx_done
+
+pop {r2-r3}
 bx lr
 
 .end
