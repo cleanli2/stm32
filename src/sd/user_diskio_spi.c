81fd3c50b2edf01b0860bc0f6234bd73aae091fa
CleanLi <clean_li@hotmail.com>
2025-08-30 23:54:27 +0800

    Revert "sd r/w log"
    This reverts commit 7c8ac0e2203e59ba067a4d3563ae279a45f686d4.

--- src/sd/user_diskio_spi.c
+++ src/sd/user_diskio_spi.c
@@ -440,7 +440,6 @@ inline DRESULT USER_SPI_read (
 	UINT count		/* Number of sectors to read (1..128) */
 )
 {
-    lprintf("sd_read\r\n");
 	if (drv || !count) return RES_PARERR;		/* Check parameter */
 	if (Stat & STA_NOINIT) return RES_NOTRDY;	/* Check if drive is ready */
 
@@ -463,7 +462,6 @@ inline DRESULT USER_SPI_read (
 	}
 	despiselect();
 
-    prt_dec(count);
 	return count ? RES_ERROR : RES_OK;	/* Return result */
 }
 
@@ -481,7 +479,6 @@ inline DRESULT USER_SPI_write (
 	UINT count			/* Number of sectors to write (1..128) */
 )
 {
-    lprintf("sd_write\r\n");
 	if (drv || !count) return RES_PARERR;		/* Check parameter */
 	if (Stat & STA_NOINIT) return RES_NOTRDY;	/* Check drive status */
 	if (Stat & STA_PROTECT) return RES_WRPRT;	/* Check write protect */
@@ -506,7 +503,6 @@ inline DRESULT USER_SPI_write (
 	}
 	despiselect();
 
-    prt_dec(count);
 	return count ? RES_ERROR : RES_OK;	/* Return result */
 }
 #endif
